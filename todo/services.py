e09aafd274ff5148a7913e5e4259431dca2244e3
ilnarka1337 <minejooker@gmail.com>
2023-06-25 18:11:54 +0400

    update filters and template

--- todo/services.py
+++ todo/services.py
@@ -1,17 +1,56 @@
 import django_filters
 from django_filters import widgets
 from django import forms
-# from django.utils.timezone import now
 from .models import Task, CHOICES
+
+from datetime import timedelta
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+
+choices = [
+    ('today', _('Сегодня')),
+    ('tomorrow', _('Завтра')),
+    ('week', _('На этой неделе')),
+    ('month', _('В этом месяце')),
+    ('year', _('В этом году')),
+]
+
+filters = {
+    'today': lambda qs, name: qs.filter(**{
+        '%s__year' % name: now().year,
+        '%s__month' % name: now().month,
+        '%s__day' % name: now().day
+    }),
+    'tomorrow': lambda qs, name: qs.filter(**{
+        '%s__year' % name: (now() + timedelta(days=1)).year,
+        '%s__month' % name: (now() + timedelta(days=1)).month,
+        '%s__day' % name: (now() + timedelta(days=1)).day,
+    }),
+    'week': lambda qs, name: qs.filter(**{
+        '%s__gte' % name: _truncate(now() - timedelta(days=7)),
+        '%s__lte' % name: _truncate(now() + timedelta(days=1)),
+    }),
+    'month': lambda qs, name: qs.filter(**{
+        '%s__year' % name: now().year,
+        '%s__month' % name: now().month
+    }),
+    'year': lambda qs, name: qs.filter(**{
+        '%s__year' % name: now().year,
+    }),
+}
+
+
+def _truncate(dt):
+    return dt.date()
 
 
 class TaskFilter(django_filters.FilterSet):
-    # status = django_filters.NumberFilter()
     status = django_filters.ChoiceFilter(choices=CHOICES, empty_label="Все", null_value=None,
                                          widget=forms.Select(attrs={"class": "form-select"}))
     start_date = django_filters.DateFilter(field_name='started_at_date', lookup_expr='gt')
     end_date = django_filters.DateFilter(field_name='started_at_date', lookup_expr='lt')
-    date_range_modify = django_filters.DateRangeFilter(field_name='started_at_date',
+    date_range_modify = django_filters.DateRangeFilter(field_name='started_at_date', empty_label="Все", null_value=None,
+                                                       choices=choices, filters=filters,
                                                        widget=forms.Select(attrs={"class": "form-select"}))
     date_range = django_filters.DateFromToRangeFilter(field_name='started_at_date',
                                                       widget=forms.Select(attrs={"class": "form-select"}))
@@ -19,36 +58,3 @@ class TaskFilter(django_filters.FilterSet):
     class Meta:
         model = Task
         fields = ['status']
-
-
-# choices = [
-#     ('today', _('Сегодня')),
-#     ('tomorrow', _('До завтра')),
-#     ('week', _('На этой неделе')),
-#     ('month', _('В этом месяце')),
-#     ('all', _('Все')),
-# ]
-#
-# filters = {
-#     'today': lambda qs, name: qs.filter(**{
-#         '%s__year' % name: now().year,
-#         '%s__month' % name: now().month,
-#         '%s__day' % name: now().day
-#     }),
-#     'yesterday': lambda qs, name: qs.filter(**{
-#         '%s__year' % name: (now() - timedelta(days=1)).year,
-#         '%s__month' % name: (now() - timedelta(days=1)).month,
-#         '%s__day' % name: (now() - timedelta(days=1)).day,
-#     }),
-#     'week': lambda qs, name: qs.filter(**{
-#         '%s__gte' % name: _truncate(now() - timedelta(days=7)),
-#         '%s__lt' % name: _truncate(now() + timedelta(days=1)),
-#     }),
-#     'month': lambda qs, name: qs.filter(**{
-#         '%s__year' % name: now().year,
-#         '%s__month' % name: now().month
-#     }),
-#     'year': lambda qs, name: qs.filter(**{
-#         '%s__year' % name: now().year,
-#     }),
-# }
